32b5115662b43236cd5c7500ce24224e7f47f67d
darraghmurphy1 <darraghmurphy77@gmail.com>
2022-07-31 15:51:00 +0000

    function to print the playing board.

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import colorama
 from colorama import Fore, Back
 colorama.init()
 
+OPPONENT_GRID = [[' '] * 5 for i in range(5)]
+USER_GRID = [[' '] * 5 for i in range(5)]
 int_letters = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
 
 
@@ -32,6 +34,14 @@ def startup_message():
 startup_message()
 
 
+def print_grid(grid):
+    """the function called to print the playing board to the terminal"""
+    print(Fore.YELLOW + '  A B C D E')
+    print(Fore.WHITE + ' ___________')
+    row_num = 1
+    for row in grid:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num += 1
+    print(Fore.RED + " ===========")
 
-OPPONENT_GRID = [[' '] * 5 for i in range(5)]
-USER_GRID = [[' '] * 5 for i in range(5)]
+
