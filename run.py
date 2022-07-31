0d014b8c19bf0944f2cefc5679ef51098043b7ba
darraghmurphy1 <darraghmurphy77@gmail.com>
2022-07-31 18:58:06 +0000

    colorama not supported on heroku so had to be removed

--- run.py
+++ run.py
@@ -1,17 +1,14 @@
 from random import randint
-import colorama 
-from colorama import Fore, Back
-colorama.init()
 
 
-OPPONENT_GRID = [[' '] * 5 for i in range(5)]
-USER_GRID = [[' '] * 5 for i in range(5)]
+OPPONENT_GRID = [['_'] * 5 for i in range(5)]
+USER_GRID = [['_'] * 5 for i in range(5)]
 int_letters = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
 
 
 def startup_message():
     """ This is the welcome message """
-    print(Fore.BLUE + """
+    print("""
 (____  \   /\  (_______(_______| |     (_______)  | |  | |   | (_____(_____ \ 
  ____)  ) /  \  _       _      | |      _____      \ \ | |__ | |  _   _____) )
 |  __  ( / /\ \| |     | |     | |     |  ___)      \ \|  __)| | | | |  ____/ 
@@ -20,17 +17,17 @@ def startup_message():
                                                                               
                                                                                                                  
     """)
-    print(Fore.BLUE + "Welcome to Battleships")
-    print(Fore.WHITE + "_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
-    print(Fore.WHITE + "_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
-    print(Fore.BLUE + "The aim of the game is to locate and bomb 5 ships")
-    print(Fore.WHITE + "_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
-    print(Fore.BLUE + "You have 15 bombs to do this")
-    print(Fore.WHITE + "_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
-    print(Fore.BLUE + "To win, destroy all 5 enemy ships")
-    print(Fore.WHITE + "_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
-    print(Fore.BLUE + "O=Miss X=Hit")
-    print(Fore.WHITE + "_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
+    print("Welcome to Battleships")
+    print("_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
+    print("+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
+    print("The aim of the game is to locate and bomb 5 ships")
+    print("_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
+    print("You have 15 bombs to do this")
+    print("_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
+    print("To win, destroy all 5 enemy ships")
+    print("_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
+    print("O=Miss X=Hit")
+    print("_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_")
 
 
 startup_message()
@@ -38,13 +35,13 @@ startup_message()
 
 def print_grid(grid):
     """the function called to print the playing board to the terminal"""
-    print(Fore.YELLOW + '  A B C D E')
-    print(Fore.WHITE + ' ___________')
+    print('  A B C D E')
+    print(' ___________')
     row_num = 1
     for row in grid:
         print("%d|%s|" % (row_num, "|".join(row)))
         row_num += 1
-    print(Fore.RED + " ===========")
+    print(" ===========")
 
 
 def spawn_ships(grid):
@@ -61,7 +58,7 @@ def spawn_ships(grid):
 def target():
     """ this function allows the user to target their desired coordinates
     validation is used to make sure the coordinates are valid."""
-    row = input(Fore.BLUE + "Enter the row you want to bomb: ")
+    row = input("Enter the row you want to bomb: ")
     print('____________________________________________')
     while row not in ["1", "2", "3", "4", "5"]:
         print('Please enter a valid co-ordinate')
@@ -103,22 +100,22 @@ if __name__ == "__main__":
         if USER_GRID[row][column] == "O":
             print("Location already hit")
         elif OPPONENT_GRID[row][column] == "X":
-            print(Fore.GREEN + "You sunk my battleship!")
+            print("You sunk my battleship!")
             print('____________________________________________')
             USER_GRID[row][column] = "X"
             bombs -= 1  
         else:
-            print(Fore.RED + "MISS! RELOAD!")
+            print("MISS! RELOAD!")
             USER_GRID[row][column] = "O"
             bombs -= 1     
         if hit_counter(USER_GRID) == 5:
-            print(Fore.GREEN + "Congratultions! You sunk my battleships.")
+            print("Congratultions! You sunk my battleships.")
             print('____________________________________________')
             break
         print("You have " + str(bombs) + " bombs left")
-        print(Fore.WHITE + '____________________________________________')
+        print('____________________________________________')
         if bombs == 0:
-            print(Fore.RED + "Game Over! You have been defeated.")
+            print("Game Over! You have been defeated.")
             print("""
             
 ███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
@@ -134,7 +131,7 @@ if __name__ == "__main__":
 ███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
 
             """)
-            print(Fore.WHITE + '____________________________________________')
+            print('____________________________________________')
 
 
-print(Back.GREEN + Fore.WHITE + "Thank you for playing.")
+print("Thank you for playing.")
